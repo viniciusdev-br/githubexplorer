b009a1dc97e58332da820441c5cea11d0817dad8
Vinicius Botelho <viniciusvcb2002@gmail.com>
2021-12-15 22:32:35 -0300

    Ambiente configurado

---
+++ src/components/RespositoryList.jsx
@@ -0,0 +1,17 @@
+export default function(){
+    return(
+        <section className="repository-list">
+            <h1>Lista de respositórios</h1>
+            <ul>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+            </ul>
+        </section>
+    )
+}
