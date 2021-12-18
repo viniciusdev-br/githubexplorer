7628297fb960f73c17598625e527fdcd8d0bc397
Vinicius Chaves Botelho <viniciusvcb2002@gmail.com>
2021-12-18 11:33:05 -0300

    Todos os arquivos modificados para tsx

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -2,8 +2,14 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss'
 import { useState, useEffect } from 'react'
 
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 export default function(){
-    const [repositories, setRespositories] = useState([])
+    const [repositories, setRespositories] = useState<Repository[]>([])
     useEffect(() => {
         fetch('https://api.github.com/users/viniciusdev-br/repos')
         .then(response => response.json())
